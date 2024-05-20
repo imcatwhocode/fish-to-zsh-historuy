f7eb0f078e341cec456bdd23cb5b801658e97ef2
Dmitry Nourell <hi@imcatwhocode.dev>
2024-05-20 18:37:25 +0300

    Init (and probably the last) commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# @imcatwhocode/fish-to-zsh-history
+
+This is a simple script that converts your fish shell history to zsh history.
+
+**This script wasn't really tested, so use it at your own risk.**
+
+## Usage
+
+Requires Bun.
+
+```sh
+git clone https://github.com/imcatwhocode/fish-to-zsh-history
+cd fish-to-zsh-history
+bun install
+bun run index.ts > ~/.zsh_history
+```
---
+++ index.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import { parse } from "yaml";
+
+interface FishHistoryEntry {
+  when: string;
+  cmd: string;
+}
+
+// Probably wouldn't work on Windows, but who really cares YOLO
+const homeDir = process.env.HOME ?? process.env.USERPROFILE ?? "~";
+
+const fishHistoryPath = resolve(homeDir, ".local/share/fish/fish_history");
+
+const fish = parse(readFileSync(fishHistoryPath, "utf8"));
+
+fish.forEach((entry: FishHistoryEntry) => {
+  const preparedCmd = entry.cmd.replaceAll("\\\\\\n", "\\\\\n");
+  process.stdout.write(`: ${entry.when}:0;${preparedCmd}\n`);
+});
